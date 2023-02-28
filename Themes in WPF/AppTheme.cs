c01fc6f7db022defb426f1505f4b35e116b9e74e
jamie <you@example.com>
2023-02-28 12:33:06 +0200

    Version 1.0

---
+++ Themes in WPF/AppTheme.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Themes_in_WPF
+{
+    class AppTheme
+    {
+
+
+
+        public static void ChangeTheme(Uri themeuri)
+        {
+            ResourceDictionary Theme = new ResourceDictionary() { Source = themeuri };
+
+            App.Current.Resources.Clear();
+            App.Current.Resources.MergedDictionaries.Add(Theme);
+
+        }
+    }
+}
